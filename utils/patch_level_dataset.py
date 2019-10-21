62f9abe3aabeef47a7909411e18769f266928744
hkjcpy <115010133@link.cuhk.edu.cn>
2019-10-21 19:12:01 +0800

    standardlize pathc level split dataset code

--- utils/patch_level_dataset.py
+++ utils/patch_level_dataset.py
@@ -1,12 +1,48 @@
-import os
-
+import argparse
 import cv2
-import skimage
+import matplotlib.pyplot as plt
 import numpy as np
+import os
 import sys
+
+
 # sys.exit(0)
 
-import matplotlib.pyplot as plt
+def ParseArguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_root_dir',
+                        type=str,
+                        default='/data/lars/data/Inbreast-dataset-cropped-pathches-connected-component-1/',
+                        help='The source data dir.')
+    
+    parser.add_argument('--data_saving_dir',
+                        type=str,
+                        default='/data/lars/models/20190920_uCs_reconstruction_connected_1_ttestlossv2_default_dilation_radius_14/',
+                        help='The dataset saved dir.')
+
+    parser.add_argument('--dataset_type',
+                        type=str,
+                        default='test',
+                        help='The type of dataset (training, validation, test).')
+
+    parser.add_argument('--patch_size',
+                        type=tuple,
+                        default=(112, 112),
+                        help='The height and width of patch.')
+
+    parser.add_argument('--patch_stride',
+                        type=int,
+                        default=56,
+                        help='The stride move from one patch to another ')
+
+    parser.add_argument('--patch_threshold',
+                        type=int,
+                        default=30000,
+                        help='patch which np.sum is lower than threshold will be discarded')
+
+    args = parser.parse_args()
+
+    return args
 
 
 def show_images(images: list) -> None:
@@ -21,7 +57,7 @@ def show_images(images: list) -> None:
 
 
 def makedir(save_dir):
-    if not os.path.isdir(save_dir):
+    if os.path.isdir(save_dir) == False:
         os.mkdir(save_dir)
         for i in ['positive', 'negative']:
             os.mkdir(os.path.join(save_dir, i))
@@ -74,7 +110,7 @@ def ExtractPatch(image, patch_size, stride):
 
 
 def save_patch(save_dir, mode, image_patch_list, label_patch_list, image_name, threshold=20000):
-    print(len(image_patch_list),len(label_patch_list),image_name)
+    print(len(image_patch_list), len(label_patch_list), image_name)
     for idx in range(len(image_patch_list)):
         image_patch = image_patch_list[idx]
         label_patch = label_patch_list[idx]
@@ -96,20 +132,18 @@ def save_patch(save_dir, mode, image_patch_list, label_patch_list, image_name, t
     return ('finish save patch')
 
 
-if __name__ == '__main__':
-    load_dir = '/data/lars/data/Inbreat_Image_splitted_10_16'
-    mysave_dir = makedir('/data/lars/data/Inbreat_patch_splitted_10_16')
+def TestPatchLevelSplit(args):
+    mysave_dir = makedir(args.data_saving_dir)
     for mod in ['training', 'validation', 'test']:
         myname_list, myimage_list, mylabel_list, mymode = LoadImage(
-            folder_dir=load_dir, mode=mod)
+            folder_dir=args.data_root_dir, mode=mod)
         for idx in range(len(myname_list)):
-            myimage_patch_list = ExtractPatch(myimage_list[idx], patch_size=(112, 112), stride=56)
-            mylabel_patch_list = ExtractPatch(mylabel_list[idx], patch_size=(112, 112), stride=56)
-            save_patch(mysave_dir, mymode, myimage_patch_list, mylabel_patch_list, myname_list[idx], threshold=20000)
+            myimage_patch_list = ExtractPatch(myimage_list[idx], patch_size=args.patch_size, stride=args.stride)
+            mylabel_patch_list = ExtractPatch(mylabel_list[idx], patch_size=args.patch_size, stride=args.stride)
+            save_patch(mysave_dir, mymode, myimage_patch_list, mylabel_patch_list, myname_list[idx],
+                       threshold=args.patch_threshold)
 
-# test_img=cv2.imread(r'C:\Users\75209\Desktop\Inbreat-Image-splitted\test\image\test57.png',cv2.IMREAD_GRAYSCALE)
-# test_label= cv2.imread(r'C:\Users\75209\Desktop\Inbreat-Image-splitted\test\labels\test57.png',cv2.IMREAD_GRAYSCALE)
-# lalist=ExtractPatch(test_label,(112,112),56)
-# plist=ExtractPatch(test_img,(112,112),56)
-# img = cv2.imread(r'C:\Users\75209\Desktop\Inbreat-Image-splitted-patch-level\positive\test\labels\positive22test26.png',cv2.IMREAD_GRAYSCALE)
-# region = measure.regionprops(img, connectivity=2)
+
+if __name__ == '__main__':
+    args = ParseArguments()
+    TestPatchLevelSplit(args)
