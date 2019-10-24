999bbe229c9ba35d575768458d0e8727b2b1831d
Lars Zhang <502463708@qq.com>
2019-10-24 21:31:41 +0800

    Bug fix & code refactoring: patch level dataset generation related code.

--- utils/patch_level_dataset_generation.py
+++ utils/patch_level_dataset_generation.py
@@ -1,4 +1,3 @@
-import argparse
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,84 +14,130 @@ def show_images(images: list) -> None:
 
     plt.show(block=True)
 
-
-def makedir(save_dir):
-    if not os.path.isdir(save_dir):
-        os.mkdir(save_dir)
-        for i in ['positive_patches', 'negative_patches']:
-            os.mkdir(os.path.join(save_dir, i))
-            for j in ['training', 'validation', 'test']:
-                os.mkdir(os.path.join(save_dir, i, j))
-                for k in ['labels', 'images']:
-                    os.mkdir(os.path.join(save_dir, i, j, k))
-
-    return save_dir
+    return
 
 
-def load_image(folder_dir, mode='test'):
-    image_dir = os.path.join(folder_dir, mode, 'images')
-    label_dir = os.path.join(folder_dir, mode, 'labels')
-    name_list = os.listdir(image_dir)
-    image_list = list()
-    label_list = list()
+def crop_patches_and_labels(image_path, label_path, patch_size, stride):
+    assert os.path.exists(image_path)
+    assert os.path.exists(label_path)
 
-    for i in range(len(name_list)):
-        img = cv2.imread(os.path.join(image_dir, name_list[i]), cv2.IMREAD_GRAYSCALE)
-        image_list.append(img)
+    image_np = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    label_np = cv2.imread(label_path, cv2.IMREAD_GRAYSCALE)
+    height, width = image_np.shape
 
-    for j in range(len(name_list)):
-        label = cv2.imread(os.path.join(label_dir, name_list[j]), cv2.IMREAD_GRAYSCALE)
-        label_list.append(label)
+    assert len(image_np.shape) == 2
+    assert image_np.shape == label_np.shape
 
-    return name_list, image_list, label_list, mode
-
-
-def extract_patch(image, patch_size, stride):
-    assert len(image.shape) == 2
-    shape = image.shape  # H, W
+    # for saving patches and labels
     image_patch_list = list()
+    label_patch_list = list()
 
-    for j in range(shape[1] // stride - 1):
-        for i in range(shape[0] // stride - 1):
-            image_patch = image[stride * i:patch_size[0] + stride * i, stride * j:patch_size[1] + stride * j]
+    for row_stride_idx in range(height // stride):
+        # start and end row index for cropping this patch
+        start_row_idx = stride * row_stride_idx
+        end_row_idx = start_row_idx + patch_size[0]
+        if end_row_idx > height - 1:
+            gap = end_row_idx - (height - 1)
+            end_row_idx -= gap
+            start_row_idx -= gap
+
+        for column_stride_idx in range(width // stride):
+            # start and end column index for cropping this patch
+            start_column_idx = stride * column_stride_idx
+            end_column_idx = start_column_idx + patch_size[1]
+            if end_column_idx > width - 1:
+                gap = end_column_idx - (width - 1)
+                end_column_idx -= gap
+                start_column_idx -= gap
+
+            # debug only
+            # print(
+            #     'row idx range: {} - {} (height = {}), column idx range: {} - {} (width = {})'.format(start_row_idx,
+            #                                                                                           end_row_idx,
+            #                                                                                           height,
+            #                                                                                           start_column_idx,
+            #                                                                                           end_column_idx,
+            #                                                                                           width))
+
+            # crop this patch and its corresponding label
+            image_patch = image_np[start_row_idx:end_row_idx, start_column_idx:end_column_idx]
+            label_patch = label_np[start_row_idx:end_row_idx, start_column_idx:end_column_idx]
+
+            # append into the lists
             image_patch_list.append(image_patch)
+            label_patch_list.append(label_patch)
 
-            if i == (shape[0] // stride - 1):
-                image_patch = image[shape[0] - patch_size[0]:shape[0], stride * j:patch_size[1] + stride * j]
-                image_patch_list.append(image_patch)
+    assert len(image_patch_list) == len(label_patch_list)
 
-        if j == (shape[1] // stride - 1):
-            image_patch = image[stride * i:patch_size[0] + stride * i, shape[1] - patch_size[1]: shape[1]]
-
-            image_patch_list.append(image_patch)
-
-    return image_patch_list
+    return image_patch_list, label_patch_list
 
 
-def save_patch(save_dir, mode, image_patch_list, label_patch_list, image_name, pixel_threshold=1,
-               area_threshold=112 * 112 * 0.5):
-    print('-------------------------------------------------------------------------------------------------------')
-    print('Processing on {}'.format(image_name))
+def filter_and_save_patches_and_labels(save_dir, dataset_type, image_patch_list, label_patch_list, image_name,
+                                       pixel_threshold=1, area_threshold=0.5):
     pos_patch_count = 0
     neg_patch_count = 0
+    other_lesion_patch_count = 0
+    background_patch_count = 0
+
     for idx in range(len(image_patch_list)):
         image_patch = image_patch_list[idx]
         label_patch = label_patch_list[idx]
-        if np.where(label_patch == 125)[0].shape[0] != 0 or np.sum(image_patch < pixel_threshold) >= area_threshold:
-            continue
-        elif np.sum(label_patch == 255) >= 0.01:
-            pos_patch_count += 1
-            dir_name = os.path.join(save_dir, 'positive_patches', mode)
-            save_name = 'positive' + str(idx) + '_' + image_name
-            cv2.imwrite(os.path.join(dir_name, 'images', save_name), image_patch)
-            cv2.imwrite(os.path.join(dir_name, 'labels', save_name), label_patch)
-        elif np.sum(label_patch) <= 0.01:
-            neg_patch_count += 1
-            dir_name = os.path.join(save_dir, 'negative_patches', mode)
-            save_name = 'negative' + str(idx) + '_' + image_name
-            cv2.imwrite(os.path.join(dir_name, 'images', save_name), image_patch)
-            cv2.imwrite(os.path.join(dir_name, 'labels', save_name), label_patch)
-    print('finish saving {} positive patches, {} negative patches. \n Totally {} patches have be saved '.format(
-        pos_patch_count, neg_patch_count, pos_patch_count + neg_patch_count))
 
-    return ('finish save patch')
+        assert image_patch.shape == label_patch.shape
+        height, width = image_patch.shape
+
+        this_patch_contain_other_lesion = False
+        if np.where(label_patch == 125)[0].shape[0] > 0:
+            this_patch_contain_other_lesion = True
+
+        this_patch_contain_enough_background = False
+        background_area_ratio = np.sum(image_patch < pixel_threshold) / (height * width)
+        if background_area_ratio >= area_threshold:
+            this_patch_contain_enough_background = True
+
+        this_is_a_positive_patch = False
+        if label_patch.max() == 255:
+            this_is_a_positive_patch = True
+
+        if this_patch_contain_other_lesion:
+            other_lesion_patch_count += 1
+            continue
+
+        if this_patch_contain_enough_background:
+            background_patch_count += 1
+            continue
+
+        if this_is_a_positive_patch:
+            pos_patch_count += 1
+
+            dst_dir = os.path.join(save_dir, 'positive_patches', dataset_type)
+            filename = 'positive' + '_' + image_name
+            filename = filename.replace('.png', '_{}.png'.format(idx))
+
+            absolute_image_dst_path = os.path.join(dst_dir, 'images', filename)
+            absolute_label_dst_path = os.path.join(dst_dir, 'labels', filename)
+
+            cv2.imwrite(absolute_image_dst_path, image_patch)
+            cv2.imwrite(absolute_label_dst_path, label_patch)
+        else:
+            neg_patch_count += 1
+
+            dst_dir = os.path.join(save_dir, 'negative_patches', dataset_type)
+            filename = 'negative' + '_' + image_name
+            filename = filename.replace('.png', '_{}.png'.format(idx))
+
+            absolute_image_dst_path = os.path.join(dst_dir, 'images', filename)
+            absolute_label_dst_path = os.path.join(dst_dir, 'labels', filename)
+
+            cv2.imwrite(absolute_image_dst_path, image_patch)
+            cv2.imwrite(absolute_label_dst_path, label_patch)
+
+    print(
+        'This image contains {} positive patches, {} negative patches, {} other_lesion_pathces, {} background_patches.'.format(
+            pos_patch_count, neg_patch_count, other_lesion_patch_count, background_patch_count))
+    print('Totally {} patches have been cropped.'.format(
+        other_lesion_patch_count + background_patch_count + pos_patch_count + neg_patch_count))
+    print('Totally {} patches have been discarded.'.format(other_lesion_patch_count + background_patch_count))
+    print('Totally {} patches have been saved.'.format(pos_patch_count + neg_patch_count))
+
+    return pos_patch_count, neg_patch_count, other_lesion_patch_count, background_patch_count
--- utils/patch_level_dataset_generation_test.py
+++ utils/patch_level_dataset_generation_test.py
@@ -1,31 +1,28 @@
 import argparse
+import os
+import shutil
 
-from utils.patch_level_dataset_generation import makedir, extract_patch, save_patch, load_image
+from utils.patch_level_dataset_generation import crop_patches_and_labels, filter_and_save_patches_and_labels
 
 
 def ParseArguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_root_dir',
+    parser.add_argument('--src_data_root_dir',
                         type=str,
-                        default='/data/lars/data/Inbreast-dataset-cropped-pathches-connected-component-1/',
-                        help='The source data dir.')
+                        default='/data/lars/data/Inbreast-roi-extracted-radiograph-level-split-dataset/',
+                        help='The source data root dir.')
 
-    parser.add_argument('--data_saving_dir',
+    parser.add_argument('--dst_data_root_dir',
                         type=str,
-                        default='/data/lars/models/20190920_uCs_reconstruction_connected_1_ttestlossv2_default_dilation_radius_14/',
-                        help='The dataset saved dir.')
-
-    parser.add_argument('--dataset_type',
-                        type=str,
-                        default='test',
-                        help='The type of dataset (training, validation, test).')
+                        default='/data/lars/data/Inbreast-patch-level-split-dataset/',
+                        help='The destination data root dir.')
 
     parser.add_argument('--patch_size',
                         type=tuple,
                         default=(112, 112),
                         help='The height and width of patch.')
 
-    parser.add_argument('--patch_stride',
+    parser.add_argument('--stride',
                         type=int,
                         default=56,
                         help='The stride move from one patch to another.')
@@ -33,46 +30,109 @@ def ParseArguments():
     parser.add_argument('--pixel_threshold',
                         type=int,
                         default=1,
-                        help='patch pixel under threshold will be treated as background.')
+                        help='Pixels whose intensity < pixel_threshold will be considered as background.')
 
     parser.add_argument('--training_area_threshold',
-                        type=int,
-                        default=112 * 112 * 0.4,
-                        help='patch pixel background area under threshold will be discarded.')
+                        type=float,
+                        default=0.6,
+                        help='The maximum background area ratio duration for training set.')
+
     parser.add_argument('--validation_area_threshold',
-                        type=int,
-                        default=112 * 112 * 0.4,
-                        help='patch pixel background area under threshold will be discarded.')
+                        type=float,
+                        default=0.6,
+                        help='The maximum background area ratio duration for validation set.')
+
     parser.add_argument('--test_area_threshold',
-                        type=int,
-                        default=112 * 112 * 0.95,
-                        help='patch pixel background area under threshold will be discarded.')
+                        type=float,
+                        default=0.1,
+                        help='The maximum background area ratio duration for test set.')
 
     args = parser.parse_args()
+
+    assert os.path.exists(args.src_data_root_dir), 'Source data root dir does not exist.'
+
+    if os.path.exists(args.dst_data_root_dir):
+        shutil.rmtree(args.dst_data_root_dir)
+    os.mkdir(args.dst_data_root_dir)
+
+    for patch_type in ['positive_patches', 'negative_patches']:
+        os.mkdir(os.path.join(args.dst_data_root_dir, patch_type))
+        for dataset_type in ['training', 'validation', 'test']:
+            os.mkdir(os.path.join(args.dst_data_root_dir, patch_type, dataset_type))
+            for image_type in ['images', 'labels']:
+                os.mkdir(os.path.join(args.dst_data_root_dir, patch_type, dataset_type, image_type))
 
     return args
 
 
-def TestPatchLevelSplit(args):
-    mysave_dir = makedir(args.data_saving_dir)
+def TestPatchLevelDatasetGeneration(args):
+    for dataset_type in ['training', 'validation', 'test']:
+        absolute_image_dir = os.path.join(args.src_data_root_dir, dataset_type, 'images')
+        filename_list = os.listdir(absolute_image_dir)
 
-    for mod in ['training', 'validation', 'test']:
-        myname_list, myimage_list, mylabel_list, mymode = load_image(
-            folder_dir=args.data_root_dir, mode=mod)
-        if mod == 'training':
-            area_threshold = args.training_area_threshold
-        elif mod == 'validation':
+        assert len(filename_list) > 0
+
+        area_threshold = args.training_area_threshold
+        if dataset_type == 'validation':
             area_threshold = args.validation_area_threshold
-        elif mod == 'test':
+        elif dataset_type == 'test':
             area_threshold = args.test_area_threshold
-        for idx in range(len(myname_list)):
-            myimage_patch_list = extract_patch(myimage_list[idx], patch_size=args.patch_size, stride=args.patch_stride)
-            mylabel_patch_list = extract_patch(mylabel_list[idx], patch_size=args.patch_size, stride=args.patch_stride)
-            save_patch(mysave_dir, mymode, myimage_patch_list, mylabel_patch_list, myname_list[idx],
-                       pixel_threshold=args.pixel_threshold, area_threshold=area_threshold)
+
+        current_idx = 0
+        pos_patch_count_dataset_level = 0
+        neg_patch_count_dataset_level = 0
+        other_lesion_patch_count_dataset_level = 0
+        background_patch_count_dataset_level = 0
+
+        for filename in filename_list:
+            current_idx += 1
+            print('---------------------------------------------------------------------------------------------------')
+            print(
+                'Processing {} out of {}: {} in {} set'.format(current_idx, len(filename_list), filename, dataset_type))
+
+            absolute_image_path = os.path.join(absolute_image_dir, filename)
+            absolute_label_path = absolute_image_path.replace('images', 'labels')
+
+            patch_list, label_list = crop_patches_and_labels(absolute_image_path, absolute_label_path,
+                                                             patch_size=args.patch_size, stride=args.stride)
+
+            pos_patch_count_image_level, \
+            neg_patch_count_image_level, \
+            other_lesion_patch_count_image_level, \
+            background_patch_count_image_level = filter_and_save_patches_and_labels(args.dst_data_root_dir,
+                                                                                    dataset_type, patch_list,
+                                                                                    label_list, filename,
+                                                                                    pixel_threshold=args.pixel_threshold,
+                                                                                    area_threshold=area_threshold)
+
+            pos_patch_count_dataset_level += pos_patch_count_image_level
+            neg_patch_count_dataset_level += neg_patch_count_image_level
+            other_lesion_patch_count_dataset_level += other_lesion_patch_count_image_level
+            background_patch_count_dataset_level += background_patch_count_image_level
+
+        print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+        print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+        print('The {} set contains {} positive patches, {} negative patches, {} other_lesion_pathces, '
+              '{} background_patches.'.format(dataset_type,
+                                              pos_patch_count_dataset_level,
+                                              neg_patch_count_dataset_level,
+                                              other_lesion_patch_count_dataset_level,
+                                              background_patch_count_dataset_level))
+        print('Totally {} patches have been cropped.'.format(other_lesion_patch_count_dataset_level +
+                                                             background_patch_count_dataset_level +
+                                                             pos_patch_count_dataset_level +
+                                                             neg_patch_count_dataset_level))
+        print('Totally {} patches have been discarded.'.format(other_lesion_patch_count_dataset_level +
+                                                               background_patch_count_dataset_level))
+        print('Totally {} patches have been saved.'.format(pos_patch_count_dataset_level +
+                                                           neg_patch_count_dataset_level))
+        print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+        print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+
+    return
 
 
 if __name__ == '__main__':
     args = ParseArguments()
 
-    TestPatchLevelSplit(args)
+    TestPatchLevelDatasetGeneration(args)
