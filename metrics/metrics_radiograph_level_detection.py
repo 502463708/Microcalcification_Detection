cfcbd48f5b92b481ff0354c25026fdc0cfaaff24
Lars Zhang <502463708@qq.com>
2019-11-18 11:38:12 +0800

    Update radiograph level metric.

--- metrics/metrics_radiograph_level_detection.py
+++ metrics/metrics_radiograph_level_detection.py
@@ -1,7 +1,10 @@
 """
 This file implements a class which can evaluate the recall and false positive
 """
+import copy
 import numpy as np
+
+from skimage import measure
 
 
 class DetectionResultRecord(object):
@@ -11,11 +14,13 @@ class DetectionResultRecord(object):
 
         self.calcification_num_dataset_level = 0
 
+        detected_num_dataset_level_list = list()
         recall_num_dataset_level_list = list()
         FP_num_dataset_level_list = list()
 
         score_threshold = 0
         while score_threshold <= 1:
+            detected_num_dataset_level_list.append(0)
             recall_num_dataset_level_list.append(0)
             FP_num_dataset_level_list.append(0)
 
@@ -23,6 +28,7 @@ class DetectionResultRecord(object):
 
         self.recall_num_dataset_level_np = np.array(recall_num_dataset_level_list)
         self.FP_num_dataset_level_np = np.array(FP_num_dataset_level_list)
+        self.detected_num_dataset_level_np = np.array(detected_num_dataset_level_list)
 
         return
 
@@ -30,6 +36,7 @@ class DetectionResultRecord(object):
         assert self.score_threshold_stride == other.score_threshold_stride
 
         self.calcification_num_dataset_level += other.calcification_num_dataset_level
+        self.detected_num_dataset_level_np += other.detected_num_dataset_level_np
         self.recall_num_dataset_level_np += other.recall_num_dataset_level_np
         self.FP_num_dataset_level_np += other.FP_num_dataset_level_np
 
@@ -37,6 +44,11 @@ class DetectionResultRecord(object):
 
     def update_calcification_num(self, calcification_num_dataset_level):
         self.calcification_num_dataset_level += calcification_num_dataset_level
+
+        return
+
+    def update_detected_num(self, threshold_idx, detected_num):
+        self.detected_num_dataset_level_np[threshold_idx] += detected_num
 
         return
 
@@ -50,40 +62,32 @@ class DetectionResultRecord(object):
 
         return
 
-    def get_detected_num(self):
-        detected_num = (self.recall_num_dataset_level_np + self.FP_num_dataset_level_np)[0]
-
-        return detected_num
-
     def print(self, logger=None):
         if logger is None:
             print('The annotated calcification number is: {}'.format(self.calcification_num_dataset_level))
-            print('The number of the detected positive calcifications is: {}'.format(self.get_detected_num()))
             score_threshold = 0
             threshold_idx = 0
             while score_threshold <= 1:
-                print('score_threshold = {:.4f}, recall_number = {}, FP_number = {}'.format(score_threshold,
-                                                                                            self.recall_num_dataset_level_np[
-                                                                                                threshold_idx],
-                                                                                            self.FP_num_dataset_level_np[
-                                                                                                threshold_idx]))
+                print('score_threshold = {:.4f}, detected_number = {}, recall_number = {}, FP_number = {}'.
+                      format(score_threshold,
+                             self.detected_num_dataset_level_np[threshold_idx],
+                             self.recall_num_dataset_level_np[threshold_idx],
+                             self.FP_num_dataset_level_np[threshold_idx]))
 
                 score_threshold += self.score_threshold_stride
                 threshold_idx += 1
         else:
             logger.write_and_print(
                 'The annotated calcification number is: {}'.format(self.calcification_num_dataset_level))
-            logger.write_and_print(
-                'The number of the detected positive calcifications is: {}'.format(self.get_detected_num()))
             score_threshold = 0
             threshold_idx = 0
             while score_threshold <= 1:
                 logger.write_and_print(
-                    'score_threshold = {:.4f}, recall_number = {}, FP_number = {}'.format(score_threshold,
-                                                                                          self.recall_num_dataset_level_np[
-                                                                                              threshold_idx],
-                                                                                          self.FP_num_dataset_level_np[
-                                                                                              threshold_idx]))
+                    'score_threshold = {:.4f}, detected_number = {}, recall_number = {}, FP_number = {}'.
+                    format(score_threshold,
+                           self.detected_num_dataset_level_np[threshold_idx],
+                           self.recall_num_dataset_level_np[threshold_idx],
+                           self.FP_num_dataset_level_np[threshold_idx]))
 
                 score_threshold += self.score_threshold_stride
                 threshold_idx += 1
@@ -123,14 +127,25 @@ class MetricsRadiographLevelDetection(object):
 
         score_threshold = 0
         threshold_idx = 0
+        filtered_residue_radiograph_np = copy.copy(processed_residue_radiograph_np)
         while score_threshold <= 1:
             processed_pred_coord_list = self.process_pred_coord_list(pred_coord_list, pred_score_list, score_threshold)
 
-            detection_result_record_radiograph_level = self.metric_a_specific_score_threshold(processed_pred_coord_list,
-                                                                                              label_coord_list,
-                                                                                              threshold_idx,
-                                                                                              detection_result_record_radiograph_level,
-                                                                                              processed_residue_radiograph_np)
+            if processed_residue_radiograph_np is None:
+                detection_result_record_radiograph_level = self.metric_a_specific_score_threshold(
+                    processed_pred_coord_list,
+                    label_coord_list,
+                    threshold_idx,
+                    detection_result_record_radiograph_level)
+            else:
+                filtered_residue_radiograph_np = self.filter_residue(filtered_residue_radiograph_np,
+                                                                     processed_pred_coord_list)
+                detection_result_record_radiograph_level = self.metric_a_specific_score_threshold(
+                    processed_pred_coord_list,
+                    label_coord_list,
+                    threshold_idx,
+                    detection_result_record_radiograph_level,
+                    filtered_residue_radiograph_np)
 
             score_threshold += self.score_threshold_stride
             threshold_idx += 1
@@ -155,7 +170,37 @@ class MetricsRadiographLevelDetection(object):
 
         return processed_pred_coord_list
 
-    def metric_a_specific_score_threshold(self, pred_coord_list, label_coord_list, threshold_idx,
+    def filter_residue(self, processed_residue_radiograph_np, processed_pred_coord_list):
+        assert len(processed_residue_radiograph_np.shape) == 2
+
+        filtered_residue_radiograph_np = copy.copy(processed_residue_radiograph_np)
+
+        residue_mask = np.zeros_like(processed_residue_radiograph_np)
+        residue_mask[processed_residue_radiograph_np > 0] = 1
+        connected_components = measure.label(residue_mask)
+        props = measure.regionprops(connected_components, coordinates='rc')
+
+        connected_idx = 0
+        if len(props) > 0:
+            for prop in props:
+                connected_idx += 1
+
+                # generate logic indexes for this connected component
+                indexes = connected_components == connected_idx
+
+                remove_this_connected_component = True
+                for processed_pred_coord in processed_pred_coord_list:
+                    if indexes[int(processed_pred_coord[0]), int(processed_pred_coord[1])]:
+                        remove_this_connected_component = False
+
+                if remove_this_connected_component:
+                    filtered_residue_radiograph_np[indexes] = 0
+
+        assert processed_residue_radiograph_np.shape == filtered_residue_radiograph_np.shape
+
+        return filtered_residue_radiograph_np
+
+    def metric_a_specific_score_threshold(self, detected_coord_list, label_coord_list, threshold_idx,
                                           detection_result_record_radiograph_level,
                                           processed_residue_radiograph_np=None):
         slack_for_recall = False
@@ -168,7 +213,7 @@ class MetricsRadiographLevelDetection(object):
             width = processed_residue_radiograph_np.shape[1]
             slack_for_recall = True
 
-        pred_num = len(pred_coord_list)
+        detected_num = len(detected_coord_list)
         label_num = len(label_coord_list)
 
         recall_num = 0
@@ -176,21 +221,20 @@ class MetricsRadiographLevelDetection(object):
 
         # for the negative patch case
         if label_num == 0:
-            FP_num = pred_num
+            FP_num = detected_num
 
         # for the positive patch case with failing to detect anything
-        elif pred_num == 0:
+        elif detected_num == 0:
             recall_num = 0
 
         # for the positive patch case with something being detected
         else:
             # calculate recall
             for label_idx in range(label_num):
-                for pred_idx in range(pred_num):
+                for detected_idx in range(detected_num):
                     label_coord = label_coord_list[label_idx]
-                    pred_coord = pred_coord_list[pred_idx]
-                    if np.linalg.norm(
-                            label_coord - pred_coord) <= self.distance_threshold:
+                    detected_coord = detected_coord_list[detected_idx]
+                    if np.linalg.norm(label_coord - detected_coord) <= self.distance_threshold:
                         recall_num += 1
                         break
                     elif slack_for_recall:
@@ -208,23 +252,24 @@ class MetricsRadiographLevelDetection(object):
 
                         for row_idx in range(crop_row_start_idx, crop_row_end_idx):
                             for column_idx in range(crop_column_start_idx, crop_column_end_idx):
-                                if np.linalg.norm(np.array([row_idx, column_idx]) - np.array(
-                                        label_coord)) <= self.distance_threshold / 2:
-                                    residue_accumulated_value += 1
+                                if np.linalg.norm(
+                                        np.array([row_idx, column_idx]) - label_coord) <= self.distance_threshold / 2:
+                                    residue_accumulated_value += processed_residue_radiograph_np[row_idx, column_idx]
 
                         if residue_accumulated_value > 0:
                             recall_num += 1
                             break
 
             # calculate FP
-            for pred_idx in range(pred_num):
+            for detected_idx in range(detected_num):
                 for label_idx in range(label_num):
                     if np.linalg.norm(
-                            label_coord_list[label_idx] - pred_coord_list[pred_idx]) <= self.distance_threshold:
+                            label_coord_list[label_idx] - detected_coord_list[detected_idx]) <= self.distance_threshold:
                         break
                     if label_idx == label_num - 1:
                         FP_num += 1
 
+        detection_result_record_radiograph_level.update_detected_num(threshold_idx, detected_num)
         detection_result_record_radiograph_level.update_recall_num(threshold_idx, recall_num)
         detection_result_record_radiograph_level.update_FP_num(threshold_idx, FP_num)
 
