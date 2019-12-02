1feeec0a927ba6944ae8df67d5963c8ad08c2626
Lars Zhang <502463708@qq.com>
2019-12-02 20:49:11 +0800

    uncertainty sta finished

--- common/uncertainty_statistics_test.py
+++ common/uncertainty_statistics_test.py
@@ -18,7 +18,7 @@ from net.vnet2d_v2 import VNet2d
 from torch.utils.data import DataLoader
 from time import time
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
 cudnn.benchmark = True
 
 
@@ -26,7 +26,7 @@ def ParseArguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_root_dir',
                         type=str,
-                        default='/data/lars/data/Inbreast-microcalcification-datasets-5764-uCs-20191107/Inbreast-patch-level-split-pos2neg-ratio-1-dataset/',
+                        default='/data/lars/data/Inbreast-microcalcification-datasets-5764-uCs-20191128/Inbreast-patch-level-split-pos2neg-ratio-1-dataset/',
                         help='Source data dir.')
     parser.add_argument('--sta_save_dir',
                         type=str,
@@ -38,7 +38,7 @@ def ParseArguments():
                         help='the number of intervals to divide uncertainty value')
     parser.add_argument('--model_saving_dir',
                         type=str,
-                        default='/data/lars/models/20191108_5764_uCs_patch_level_reconstruction_ttestlossv3_default_dilation_radius_7/',
+                        default='/data/lars/models/models/20191129_5764_uCs_patch_level_reconstruction_ttestlossv3_default_dilation_radius_7/',
                         help='Model saved dir.')
     parser.add_argument('--epoch_idx',
                         type=int,
@@ -46,7 +46,7 @@ def ParseArguments():
                         help='The epoch index of ckpt, set -1 to choose the best ckpt on validation set.')
     parser.add_argument('--mc_epoch_indexes',
                         type=int,
-                        default=[410, 420, 430, 440, 450, 460, 470, 480, 490, 500],
+                        default=[580, 500, 520, 540, 560, 580, 600, 620, 640, 660, 680],
                         help='The epoch ckpt index list for generating uncertainty maps'
                              'set null list [] to switch off.')
     parser.add_argument('--dataset_type',
@@ -75,7 +75,7 @@ def ParseArguments():
                         help='The bool variable for slacking recall metric standard.')
     parser.add_argument('--batch_size',
                         type=int,
-                        default=48,
+                        default=30,
                         help='Batch size for evaluation.')
     parser.add_argument('--save_nii',
                         type=bool,
@@ -87,8 +87,12 @@ def ParseArguments():
     return args
 
 
-def pltsave(data, bins, dir, name):
-    plt.hist(data, bins=bins, range=(0, 1))
+def pltsave(hist, dir, name):
+    objects = np.linspace(0, 1, len(hist))
+    y_pos = np.arange(len(objects))
+    plt.bar(y_pos, hist)
+    plt.xticks(np.array([0, 200, 400, 600, 800, 1000]), np.array([0, 0.04, 0.08, 0.12, 0.16, 0.2]))
+    plt.ylabel('uncertainty value')
     plt.title("{} distribution".format(name))
     plt.savefig(os.path.join(dir, '{}.png'.format(name)))
     plt.close()
@@ -98,7 +102,13 @@ def UncertaintySTA(args):
     prediction_saving_dir = os.path.join(args.model_saving_dir,
                                          'reconstruction_results_dataset_{}_epoch_{}'.format(args.dataset_type,
                                                                                              args.epoch_idx))
-
+    # initialize logger
+    if os.path.exists(args.sta_save_dir):
+        shutil.rmtree(args.sta_save_dir)
+    os.mkdir(args.sta_save_dir)
+    logger = Logger(args.sta_save_dir, 'uncertainty_distribution_sta.txt')
+    logger.write_and_print('Dataset: {}'.format(args.data_root_dir))
+    logger.write_and_print('Dataset type: {}'.format(args.dataset_type))
     # define the network
     network = VNet2d(num_in_channels=cfg.net.in_channels, num_out_channels=cfg.net.out_channels)
 
@@ -129,6 +139,7 @@ def UncertaintySTA(args):
         net_list = get_net_list(network, ckpt_dir, args.mc_epoch_indexes, logger)
 
     # create dataset
+
     dataset = MicroCalcificationDataset(data_root_dir=args.data_root_dir,
                                         mode=args.dataset_type,
                                         enable_random_sampling=False,
@@ -136,6 +147,7 @@ def UncertaintySTA(args):
                                         image_channels=cfg.dataset.image_channels,
                                         cropping_size=cfg.dataset.cropping_size,
                                         dilation_radius=args.dilation_radius,
+                                        load_uncertainty_map=False,
                                         calculate_micro_calcification_number=cfg.dataset.calculate_micro_calcification_number,
                                         enable_data_augmentation=False)
 
@@ -146,11 +158,11 @@ def UncertaintySTA(args):
     metrics = MetricsReconstruction(args.prob_threshold, args.area_threshold, args.distance_threshold,
                                     args.slack_for_recall)
 
-    all_positive_uncertainty_in_dataset = np.array([])
-    tp_uncertainty_in_dataset = np.array([])
-    fn_uncertainty_in_dataset = np.array([])
+    all_positive_uncertainty_in_dataset = np.zeros(args.bins)
+    tp_uncertainty_in_dataset = np.zeros(args.bins)
+    fn_uncertainty_in_dataset = np.zeros(args.bins)
 
-    for batch_idx, (images_tensor, pixel_level_labels_tensor, pixel_level_labels_dilated_tensor,
+    for batch_idx, (images_tensor, pixel_level_labels_tensor, pixel_level_labels_dilated_tensor, _,
                     image_level_labels_tensor, _, filenames) in enumerate(data_loader):
         # transfer the tensor into gpu device
         images_tensor = images_tensor.cuda()
@@ -161,36 +173,43 @@ def UncertaintySTA(args):
         # MC dropout
         uncertainty_maps_np = generate_uncertainty_maps(net_list, images_tensor) if calculate_uncertainty else None
 
-        # evaluation    pixel_level_labels_dilated_tensor, post_Process_preds_np, uncertainty_maps_np
         # in tp, fn label area  uncertainty value distribution
         post_process_preds_np, calcification_num_batch_level, recall_num_batch_level, FP_num_batch_level, \
         result_flag_list = metrics.metric_batch_level(prediction_residues_tensor, pixel_level_labels_tensor)
 
-        pixel_level_labels_dilated_np = pixel_level_labels_dilated_tensor.view(-1).numpy()
+        pixel_level_labels_dilated = pixel_level_labels_dilated_tensor.view(-1).numpy()
         preds_positive = post_process_preds_np.reshape(-1)
-        uncertainty_maps_np = uncertainty_maps_np.reshape(-1)
+        uncertainty_maps = uncertainty_maps_np.reshape(-1)
 
-        all_positive_uncertainty = np.multiply(pixel_level_labels_dilated_np, uncertainty_maps_np)
-        all_positive_uncertainty = all_positive_uncertainty[all_positive_uncertainty != 0]
-        all_positive_uncertainty_in_dataset = np.append(all_positive_uncertainty_in_dataset, all_positive_uncertainty)
+        all_positive_uncertainty_batch = uncertainty_maps[pixel_level_labels_dilated == 1]
+        all_positive_uncertainty_distr_batch, _ = np.histogram(all_positive_uncertainty_batch,
+                                                               bins=args.bins, range=(0, 0.2))
+        all_positive_uncertainty_in_dataset += all_positive_uncertainty_distr_batch
 
-        tp_location = np.multiply(pixel_level_labels_dilated_np, preds_positive)
-        fn_location = pixel_level_labels_dilated_np - tp_location
-        tp_uncertainty = np.multiply(tp_location, uncertainty_maps_np)
-        tp_uncertainty = tp_uncertainty[tp_uncertainty != 0]
-        tp_uncertainty_in_dataset = np.append(tp_uncertainty_in_dataset, tp_uncertainty)
+        tp_location = np.multiply(pixel_level_labels_dilated, preds_positive)
+        fn_location = np.zeros_like(preds_positive)
+        fn_location[pixel_level_labels_dilated == 1] = 1
+        fn_location[preds_positive == 1] = 0
 
-        fn_uncertainty = np.multiply(fn_location, uncertainty_maps_np)
-        fn_uncertainty = fn_uncertainty[fn_uncertainty != 0]
-        fn_uncertainty_in_dataset = np.append(fn_uncertainty_in_dataset, fn_uncertainty)
+        tp_uncertainty_batch = uncertainty_maps[tp_location == 1]
+        tp_uncertainty_distr_batch, _ = np.histogram(tp_uncertainty_batch, bins=args.bins, range=(0, 0.2))
+        tp_uncertainty_in_dataset += tp_uncertainty_distr_batch
 
-    if os.path.exists(args.sta_save_dir):
-        shutil.rmtree(args.sta_save_dir)
-    os.mkdir(args.sta_save_dir)
+        fn_uncertainty_batch = uncertainty_maps[fn_location == 1]
+        fn_uncertainty_distr_batch, _ = np.histogram(fn_uncertainty_batch, bins=args.bins, range=(0, 0.2))
+        fn_uncertainty_in_dataset += fn_uncertainty_distr_batch
+    print(all_positive_uncertainty_in_dataset[0:5])
+    print(tp_uncertainty_in_dataset[0:5])
+    print(fn_uncertainty_in_dataset[0:5])
 
-    pltsave(all_positive_uncertainty_in_dataset, bins=args.bins, dir=args.sta_save_dir, name='all positive uncertainty')
-    pltsave(tp_uncertainty_in_dataset, bins=args.bins, dir=args.sta_save_dir, name='True Positive uncertainty')
-    pltsave(fn_uncertainty_in_dataset, bins=args.bins, dir=args.sta_save_dir, name='False Negative uncertainty')
+
+    all_positive_uncertainty_in_dataset[all_positive_uncertainty_in_dataset > 2000] = 2000
+    tp_uncertainty_in_dataset[tp_uncertainty_in_dataset > 2000] = 2000
+    fn_uncertainty_in_dataset[fn_uncertainty_in_dataset > 2000] = 2000
+
+    pltsave(all_positive_uncertainty_in_dataset, dir=args.sta_save_dir, name='all positive uncertainty')
+    pltsave(tp_uncertainty_in_dataset, dir=args.sta_save_dir, name='True Positive uncertainty')
+    pltsave(fn_uncertainty_in_dataset, dir=args.sta_save_dir, name='False Negative uncertainty')
 
     return
 
