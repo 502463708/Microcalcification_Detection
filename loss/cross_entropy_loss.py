d84422b5af0d28046aa6dff0a153c5163e8863f8
Lars Zhang <502463708@qq.com>
2019-12-30 23:07:30 +0800

    CE modified and its test code implemented.

--- loss/cross_entropy_loss.py
+++ loss/cross_entropy_loss.py
@@ -13,26 +13,35 @@ class CrossEntropyLoss(nn.Module):
 
         return
 
-    def forward(self, preds, pixel_level_labels, logger=None):
+    def forward(self, preds, labels, logger=None):
         assert torch.is_tensor(preds)
-        assert torch.is_tensor(pixel_level_labels)
-        assert len(preds.shape) == 4  # shape: B, C, H, W
-        assert len(pixel_level_labels.shape) == 3  # shape: B, H, W
+        assert torch.is_tensor(labels)
+        assert 2 <= len(preds.shape) <= 5
+        assert len(labels.shape) == len(preds.shape) - 1
 
-        # pixel_level_labels must be a tensor on gpu devices
-        if pixel_level_labels.device.type != 'cuda':
-            pixel_level_labels = pixel_level_labels.cuda()
+        num_dimensions = len(preds.shape) - 2  # 0D, 1D, 2D, or 3D
+        num_channels = preds.shape[1]
+
+        # labels must be a tensor on gpu devices
+        if labels.device.type != 'cuda':
+            labels = labels.cuda()
 
         # reshape preds into vectors
-        preds = preds.permute(0, 2, 3, 1).contiguous()
-        preds = preds.view(-1, 2)
+        if num_dimensions == 1:
+            preds = preds.permute(0, 2, 1).contiguous()
+        elif num_dimensions == 2:
+            preds = preds.permute(0, 2, 3, 1).contiguous()
+        elif num_dimensions == 3:
+            preds = preds.permute(0, 2, 3, 4, 1).contiguous()
+
+        preds = preds.view(-1, num_channels)
 
         # reshape pixel_level_labels into vectors
-        pixel_level_labels = pixel_level_labels.view(-1)
+        labels = labels.view(-1)
 
-        assert preds.shape[0] == pixel_level_labels.shape[0]
+        assert preds.shape[0] == labels.shape[0]
 
-        loss = self.loss_func(preds, pixel_level_labels)
+        loss = self.loss_func(preds, labels)
 
         return loss
 
